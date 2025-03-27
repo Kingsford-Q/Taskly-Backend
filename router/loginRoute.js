8d69f012819062370753a5510a36c7adb3726fb1
Kingsford-Q <158656114+Kingsford-Q@users.noreply.github.com>
2025-03-27 10:01:27 +0000

    Login Routes fixing

--- router/loginRoute.js
+++ router/loginRoute.js
@@ -5,6 +5,10 @@ import loginController from '../controllers/loginController.js'
 
 const loginRoute = express.Router();
 
-loginRoute.post('/login', loginController);
+loginRoute.post('/login', (req, res, next) => {
+    console.log("Login Route Hit");
+    console.log("Request Body:", req.body);
+    next();
+}, loginController);
 
 export default loginRoute;
